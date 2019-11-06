d3c620242c545158e46dfc1e8888a54f9225ba9a
itsevalieu <itsevalieu@gmail.com>
2019-11-05 22:58:24 -0800

    Create routes for Home and Closet views

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import App from './App';
+import Home from './components/Home/Home';
+import Closet from './components/Closet/Closet';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Routes = () => {
+    return (
+        <Router>
+            <App>
+                <Switch>
+                    <Route exact path='/' render={() => <Home />} />
+                    <Route exact path='/closet' render={() => <Closet />} />
+                </Switch>
+            </App>
+        </Router>
+    );
+}
+
+ReactDOM.render(<Routes />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
